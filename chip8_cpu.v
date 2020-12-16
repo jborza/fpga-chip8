2cf52a571ea3bc5277a6584bef86725ec00958b0
jborza <jborza@gmail.com>
2020-12-16 20:34:48 +0100

    CPU instruction handlers

--- chip8_cpu.v
+++ chip8_cpu.v
@@ -1,9 +1,14 @@
 module chip8_cpu(
 	input wire clk,
 	input wire reset,
-	output reg[11:0] out_address,
-	input wire [7:0] in_data
+	output reg[11:0] address_out,
+	input wire [7:0] data_in,
+	output reg write_enable,
+	output reg [7:0] data_out,
+	input wire [15:0] keys
 );
+
+`include "chip8_cpu_opcodes.vh"
 
 //display RAM (
 //chip-8 RAM
@@ -17,10 +22,11 @@ module chip8_cpu(
 //////////////////
 // CPU register file
 
-reg [15:0] reg_I;
-reg [11:0] PC; //should be 12-bit only
-reg [7:0] reg_V[3:0]; //V0..VF
-reg [15:0] stack[3:0]; //16-word stack
+reg [15:0] I;
+reg [15:0] PC = 12'h200; //could be 12-bit only
+reg [7:0] reg_V[15:0]; //V0..VF
+reg [15:0] stack[15:0]; //16-word stack
+reg [3:0] SP; //stack pointer
 reg [7:0] delay_timer;
 reg [7:0] sound_timer;
 	
@@ -31,7 +37,7 @@ localparam CPU_TICKS_PER_SECOND = 540;
 localparam CLOCKS_PER_CPU_TICK = 50_000_000 / CPU_TICKS_PER_SECOND;
 localparam CPU_TICKS_PER_TIMER_TICK = 9; //540/60
 
-reg[31:0] tick_counter;
+reg [31:0] tick_counter;
 
 reg [7:0] vx;
 reg [7:0] vy;
@@ -50,13 +56,19 @@ wire [2:0] alu_op;
 reg cpu_tick;
 wire [3:0] op_main;
 wire [3:0] op_sub;
+wire [7:0] alu_out;
+wire [7:0] alu_carry;
+
+// pseudorandom generator output
+wire [7:0] lfsr_out;
 
 //CPU states
 localparam [7:0] 
 	state_fetch_1 = 7'h0,
 	state_fetch_2 = 7'h1,
 	state_fetch_end = 7'h2,
-	state_decode = 7'h3;
+	state_decode = 7'h3,
+	state_execute = 7'h4;
 	
 reg [7:0] state;
 
@@ -74,28 +86,21 @@ decoder decoder(
 );
 
 ALU alu(
-	.X(alu_x),
-	.Y(alu_y),
+	.X(vx),
+	.Y(vy),
 	.operation(alu_op),
 	.out(alu_out),
 	.carry_out(alu_carry)
 );
 
-//    case 0xA000:
-//        //set index register to NNN
-//        state->I = opcode & 0x0FFF;
-//        break;
-//		  
-//	    case 0x1000:
-//        //jump to NNN
-//        state->PC = opcode & 0x0FFF;
-//        break;
+lfsr lfsr(
+	.clk(clk),
+	.enable(1'b1),
+	.out(lfsr_out)
+);
 
-//always @* begin
-//	vx = opcode[11:8];
-//	vy = opcode[7:4];
-//	nnn = opcode[11:0];
-//end
+//TODO reset the registers on reset signal
+
 
 //cpu tick enabler
 always @(posedge clk) begin
@@ -113,43 +118,156 @@ end
 always @(posedge clk) begin
 	if(cpu_tick) begin
 		if(delay_timer > 0)
-			delay_timer <= delay_timer - 1;
+			delay_timer <= delay_timer - 1'b1;
 		if(sound_timer > 0)
-			sound_timer <= sound_timer - 1;
+			sound_timer <= sound_timer - 1'b1;
 	end
 end
 
 //TODO see https://github.com/asinghani/pifive-cpu/blob/main/cpu/rtl/decode/decode.sv
 always @(posedge clk) begin 
 	if(cpu_tick) begin
-	
+		write_enable <= 1'b1;
 		//fetch, decode, execute
 		//fetch byte 1
 		case(state)
 				state_fetch_1:
 				begin
 					//MAR = PC
-					out_address <= PC;
+					address_out <= PC;
 					state <= state_fetch_2;
-					PC <= PC + 1;
+					PC <= PC + 1'b1;
 				end
 				state_fetch_2: 
 				begin
-					out_address <= PC;
-					PC <= PC + 1;
+					address_out <= PC;
+					PC <= PC + 1'b1;
 					//store first half of opcode 
-					opcode[15:8] <= in_data;
+					opcode[15:8] <= data_in;
+					state <= state_fetch_end;
 				end
 				state_fetch_end: 
 				begin
 					//store second half of opcode
-					opcode [7:0] = in_data;										
+					opcode [7:0] = data_in;	
+					state <= state_decode;
 				end
 				state_decode:
 				begin
 					//retrieve vx, vy
-					vx <= reg_V[x];
-					vy <= reg_V[y];
+					vx = reg_V[x];
+					vy = reg_V[y];
+					state <= state_execute;
+					//do stuff based on the main and sub opcode
+				end
+				state_execute:
+				begin
+					case (op_main)
+						4'h8: //ALU
+							begin
+							reg_V[x] <= alu_out;
+							reg_V[15] <= alu_carry;
+							end
+						4'h0: // display / flow
+							case (op_sub)
+								4'h0: begin
+									//TODO clear screen
+								end
+								4'h1: // return from subroutine
+								begin
+									PC <= stack[SP-1'b1];
+									SP <= SP - 1'b1;
+								end							
+							endcase
+						4'h1: //goto NNN
+							PC <= nnn;
+						4'h2: //call NNN
+						begin
+							// call subroutine
+							stack[SP] <= PC;
+							SP <= SP + 1'b1;
+							PC <= nnn;
+						end
+						4'h3: //condition - if(vx==NN) skip next
+						begin
+							if(vx==nn) begin
+								PC <= PC + 2'd2;
+							end
+						end
+						4'h4: // if(vx != NN)
+						begin
+							if(vx!=nn) begin
+								PC <= PC + 2'd2;
+							end
+						end
+						4'h5: // if(vx == vy)
+						begin
+							if(vx==vy) begin
+								PC <= PC + 2'd2;
+							end
+						end
+						4'h6: // vx = nn
+							reg_V[x] <= nn;
+						4'h7: // vx += nn
+							reg_V[x] <= reg_V[x] + nn;
+						4'hA:
+							I <= nnn;
+						4'hB:
+							PC <= reg_V[0] + nnn;
+						4'hC: // random & NN
+							reg_V[0] <= lfsr_out & nn;
+						//TODO 4'hD draw
+						4'hD:
+							reg_V[3] <= I; //DUMMY assignment
+						//TODO 4'hE
+						4'hE:
+							case (op_sub)
+								O_E_KEY: //if(key()==Vx)
+								begin
+									PC <= PC + 2'd2;
+								end
+								O_E_KEY_NOT: //if(key()!=Vx)
+								begin
+									PC <= PC + 2'd2;
+								end
+							endcase
+						4'hF:
+							case (op_sub)
+								O_FX07:
+									reg_V[x] <= delay_timer;
+								O_FX0A:
+									//TODO get key, also don't advance PC if it's not pressed
+									reg_V[x] <= 8'hFF;
+								O_FX15:
+									//TODO delay timer - avoid clash with delay timer decrementer
+//									delay_timer <= vx;
+								O_FX18:
+									sound_timer <= vx;
+								O_FX1E: // I += Vx
+									I <= I + vx;
+								O_FX29: //TODO sprites
+									I <= 8'hFF;
+								O_FX33: //TODO BCD - 3/4 step FSM
+									begin
+									address_out <= I;
+									data_out <= 8'hFF;
+									write_enable <= 1'b1;
+									end
+								O_FX55: //reg_dump - FSM
+									begin
+									address_out <= I;
+									write_enable <= 1'b0;
+									data_out <= reg_V[x]; //0 to X
+									end
+								O_FX65: //reg load - FSM
+									begin
+									address_out <= I;
+									//TODO increase offset
+									reg_V[x] <= data_in; //0 to X
+									end
+							endcase
+							
+					endcase
 				end
 					//determine next state based on the opcode
 //				default:
--- fpga_chip8.v
+++ fpga_chip8.v
@@ -18,19 +18,38 @@ always@(posedge clk) begin
 end
 
 // memory wires
-reg we; //ram write enable
-reg [9:0] write_address;
+// framebuffer
+wire fb_we; //ram write enable
+wire [9:0] fb_write_address;
+wire [9:0] fb_read_address;
+wire [7:0] fb_ram_in;
+wire [7:0] fb_ram_out;
+// ram
+wire we; //ram write enable
+wire [9:0] write_address;
 wire [9:0] read_address;
-reg [7:0] ram_in;
+wire [7:0] ram_in;
 wire [7:0] ram_out;
+
 wire [7:0] ascii_scan_data;
-reg [9:0] current_address = 6'h0;
+
+wire [15:0] keys; //TODO wire to the debounced keypad
+//reg [9:0] current_address = 6'h0;
 
 // display pixel buffer
-ram ram(
+ram framebuffer(
    .clk(clk),
-	.q(ram_out), 
-	.d(ram_in), 
+	.q(fb_ram_out), 
+	.d(fb_ram_in), 
+	.write_address(fb_write_address), 
+	.read_address(fb_read_address), 
+	.we(fb_we)
+);
+
+chip8_ram ram(
+	.clk(clk),
+	.q(ram_out),
+	.d(ram_in),
 	.write_address(write_address), 
 	.read_address(read_address), 
 	.we(we)
@@ -43,13 +62,19 @@ LCD12864 lcd(
 		.rw(rw), 
 		.en(en), 
 		.dat(dat),
-		.address_out(read_address),
-		.data_in(ram_out)
+		.address_out(fb_read_address),
+		.data_in(fb_ram_out)
 );
 
 chip8_cpu cpu(
 	.clk(clk),
-	.reset(~reset)
+	.reset(~reset),
+	//.address_out(write_address),
+	.address_out(read_address),
+	.data_in(ram_out),
+	.write_enable(we),
+	.data_out(ram_in),
+	.keys(keys)
 );
 
 endmodule
