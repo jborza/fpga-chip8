1ce421260474e94ad7a84373f5ec7df459eb7215
jborza <jborza@gmail.com>
2020-12-17 08:05:16 +0100

    LCD renderer - decreased the output offset by 1

--- renderer.v
+++ renderer.v
@@ -13,6 +13,7 @@ module renderer(
 
 reg running;
 reg [9:0] write_address;
+reg [9:0] write_address_previous;
 reg [11:0] read_address;
 
 always @(posedge clk)
@@ -34,7 +35,8 @@ begin
 		fb_write_enable <= 'b1;
 		main_ram_read_address <= read_address;
 		//((write_address / 8) * 32 )  + (write_address % 8)
-		fb_write_address <= {write_address[7:3], 2'b00 ,  write_address[2:0]};		
+		fb_write_address <= {write_address_previous[7:3], 2'b00 ,  write_address_previous[2:0]};	
+		write_address_previous <= write_address;
 		if(read_address == 'h1ff) //'h1ff)
 		begin
 			running <= 'b0;
